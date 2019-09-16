84f1b3f48abdba5f60f73a25101cd84dc5c378d8
Harrison Chacon <Harrisonlchr@gmail.com>
2019-09-16 17:01:23 -0300

    Agregado de posts a la pag, mediante js simulando recepcion por post

--- www/curso-javascript/proyecto-js/js/main.js
+++ www/curso-javascript/proyecto-js/js/main.js
@@ -1,1 +1,79 @@
-'use strict'
+$(document).ready(function () {
+    alert("VAMOS");
+
+    var posts = [
+      {
+        title: 'Titulo 1',
+        date: new Date(),
+        parrafo: `'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Phasellus dui ante, rutrum ultrices tellus pellentesque,
+        mollis auctor nunc. Sed mollis felis neque, quis viverra
+        justo commodo vitae. Orci varius natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Suspendisse
+        potenti. Suspendisse efficitur aliquam velit.
+        Donec vehicula risus augue, luctus venenatis ante
+        sollicitudin a. Curabitur sit amet felis massa.'`
+      },
+      {
+        title: 'Titulo 2',
+        date: new Date(),
+        parrafo: `'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Phasellus dui ante, rutrum ultrices tellus pellentesque,
+        mollis auctor nunc. Sed mollis felis neque, quis viverra
+        justo commodo vitae. Orci varius natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Suspendisse
+        potenti. Suspendisse efficitur aliquam velit.
+        Donec vehicula risus augue, luctus venenatis ante
+        sollicitudin a. Curabitur sit amet felis massa.'`
+      },
+      {
+        title: 'Titulo 3',
+        date: new Date(),
+        parrafo: `'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Phasellus dui ante, rutrum ultrices tellus pellentesque,
+        mollis auctor nunc. Sed mollis felis neque, quis viverra
+        justo commodo vitae. Orci varius natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Suspendisse
+        potenti. Suspendisse efficitur aliquam velit.
+        Donec vehicula risus augue, luctus venenatis ante
+        sollicitudin a. Curabitur sit amet felis massa.'`
+      },
+      {
+        title: 'Titulo 4',
+        date: new Date(),
+        parrafo: `'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Phasellus dui ante, rutrum ultrices tellus pellentesque,
+        mollis auctor nunc. Sed mollis felis neque, quis viverra
+        justo commodo vitae. Orci varius natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Suspendisse
+        potenti. Suspendisse efficitur aliquam velit.
+        Donec vehicula risus augue, luctus venenatis ante
+        sollicitudin a. Curabitur sit amet felis massa.'`
+      },
+
+    ];
+
+    posts.forEach((elemento, index)=>{
+      var post = `
+      <article class="post">
+        <h2>${elemento.title}</h2>
+        <span class="date">${elemento.date}</span>
+        <p>${elemento.parrafo}</p>
+      </article>
+
+      `;
+      $("#posts").append(post);
+    });
+
+
+    $(".subir").click(function(e){
+      e.preventDefault();
+
+      $('html, body').animate({
+        scrollTop: 0
+      }, 500);
+
+      return false;
+    });
+
+  });
