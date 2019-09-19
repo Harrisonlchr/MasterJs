899243978dc7aae91d2a044c246d8055355f85c7
Harrison Chacon <Harrisonlchr@gmail.com>
2019-09-19 17:58:46 -0300

    Configurando el Routing de Angular

--- www/curso-javascript/aprendiendo-angular/src/app/app.module.ts
+++ www/curso-javascript/aprendiendo-angular/src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {routing, appRoutingProviders } from './app.routing';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideojuegoComponet} from './videojuego/videojuego.component';
 import { ZapatillasComponent} from './zapatillas/zapatillas.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideojuegoComponet,
     ZapatillasComponent,
-    CursosComponent
+    CursosComponent,
+    HomeComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ www/curso-javascript/aprendiendo-angular/src/app/app.routing.ts
@@ -0,0 +1,27 @@
+// Importar modulos del router de AprendiendoAngular
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+// importar componentes
+
+import { VideojuegoComponet} from './videojuego/videojuego.component';
+import { ZapatillasComponent} from './zapatillas/zapatillas.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+
+// Array de rutas
+const appRoutes: Routes = [
+
+  {path: '', component: HomeComponent},
+  {path: 'videojuego', component: VideojuegoComponet},
+  {path: 'zapatillas', component: ZapatillasComponent},
+  {path: 'cursos', component: CursosComponent},
+  {path: '**', component: HomeComponent},
+
+];
+
+// Exportar el modulo del touter
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
