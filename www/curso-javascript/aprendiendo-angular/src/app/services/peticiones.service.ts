7ba2ed5ba085456f2defb1def45ed84c86349437
Harrison Chacon <Harrisonlchr@gmail.com>
2019-09-23 10:22:01 -0300

    Implementacion de backend con NodeJS mongoose

--- www/curso-javascript/aprendiendo-angular/src/app/services/peticiones.service.ts
+++ www/curso-javascript/aprendiendo-angular/src/app/services/peticiones.service.ts
@@ -16,6 +16,12 @@ export class PeticionesService{
       return this._http.get(this.url + 'api/users/' + userId);
     }
 
+    addUser(user): Observable<any>{
+      let params = JSON.stringify(user);
+      let headers = new HttpHeaders().set('content-Type', 'application/json');
+
+      return this._http.post(this.url+'/api/users', params, {headers: headers});
+    }
 
 
 }
