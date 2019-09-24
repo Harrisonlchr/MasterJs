99ba4c0b84d22ec86dcce78da8e206300fa2704b
Harrison Chacon <Harrisonlchr@gmail.com>
2019-09-24 16:19:35 -0300

    Creando metodos para obtener y listar informacion desde la DDBB

--- www/curso-javascript/backend/controllers/project.js
+++ www/curso-javascript/backend/controllers/project.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var Project = require('../models/project');
 
 var controller = {
   home: function(req, res){
@@ -7,11 +8,46 @@ var controller = {
       message: 'Soy la home'
     });
   },
+
   test: function(req, res){
     return res.status(200).send({
       message: 'Soy el metodo test del controlador'
     });
+  },
+  saveProject: function(req, res){
+    var project = new Project();
+
+    var params = req.body;
+    project.name = params.name;
+    project.description = params.description;
+    project.category = params.category;
+    project.year = params.year;
+    project.langs = params.langs;
+    project.image = null;
+
+    project.save((err, projectStored) => {
+      if (err) return res.status(500).send({message: 'Error al guardar el documento'});
+
+      if (!projectStored) return res.status(404).send({message: 'No se ha podido guardar el proyecto'});
+
+      return res.status(200).send({project: projectStored});
+
+
+    });
+  },
+
+  getProjects: function (req, res) {
+    Project.find({}).sort('-year').exec((err, projects) => {
+
+      if (err) return res.status(500).send({message: 'Error al devolver los datos'});
+
+      if (!projects) return res.status(404).send({message: 'No hay proyectos para mostrar'});
+
+      return res.status(200).send({projects});
+    })
+
   }
+
 };
 
 module.exports = controller;
--- www/curso-javascript/backend/routes/project.js
+++ www/curso-javascript/backend/routes/project.js
@@ -7,5 +7,7 @@ var router = express.Router();
 
 router.get('/home', projectController.home);
 router.post('/test', projectController.test);
+router.post('/save-project', projectController.saveProject);
+router.get('/projects', projectController.getProjects);
 
 module.exports = router;
